8a78a70c53078643183202cfb6ef62d7b3c84ca4
Nicolò Catalani <Nicol Catalani>
2017-01-15 16:41:14 +0100

    added class Studente

---
+++ Esercitazione/Studente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercitazione
+{
+    class Studente: Persona
+    {
+        public string sezione { get; set; }
+
+        public Studente(string nome, string cognome, int anni, string sezione): base(nome, cognome, anni)
+        {
+            this.sezione = sezione;
+        }
+
+        public override string describe()
+        {
+            return base.describe() + "\r\n" + "Sezione: " + sezione;
+        }
+    }
+}
